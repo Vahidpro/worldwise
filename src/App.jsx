754afe328e3fcc71c3ec1e3da5563372db73e9be
Vahidpro <ivahidp@gmail.com>
2024-01-23 18:27:08 +0330

    Added basic navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
-import PageNotFound from "./pages/Homepage copy";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
 	return (
