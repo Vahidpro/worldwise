4fc8c2fb996b4d9af5c47f2fcf12e9d5f58bd967
Vahidpro <ivahidp@gmail.com>
2024-01-23 18:09:49 +0330

    Added basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+import PageNotFound from "./pages/Homepage copy";
+
 function App() {
-	return <div>WorldWise</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path="/"
+					element={<Homepage />}
+				/>
+				<Route
+					path="/pricing"
+					element={<Pricing />}
+				/>
+				<Route
+					path="/product"
+					element={<Product />}
+				/>
+			</Routes>
+			<Routes
+				path="*"
+				element={<PageNotFound />}
+			></Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
