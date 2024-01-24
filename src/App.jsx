084170e17fb5a76799484f3c63f5dc78f82441df
Vahidpro <ivahidp@gmail.com>
2024-01-24 16:05:05 +0330

    Implemented cities list, routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,30 @@ import Product from "./pages/Product";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
 
+const BASE_URL = "http://localhost:9000";
 function App() {
+	const [cities, setCities] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(() => {
+		async function fetchCities() {
+			try {
+				setIsLoading(true);
+				const res = await fetch(`${BASE_URL}/cities`);
+				const data = await res.json();
+				setCities(data);
+			} catch (error) {
+				alert("There was an error loading data...");
+			} finally {
+				setIsLoading(false);
+			}
+		}
+		fetchCities();
+	}, []);
+
 	return (
 		<BrowserRouter>
 			<Routes>
@@ -32,11 +54,21 @@ function App() {
 				>
 					<Route
 						index
-						element={<p>List of cities</p>}
+						element={
+							<CityList
+								cities={cities}
+								isLoading={isLoading}
+							/>
+						}
 					/>
 					<Route
 						path="cities"
-						element={<p>List of cities</p>}
+						element={
+							<CityList
+								cities={cities}
+								isLoading={isLoading}
+							/>
+						}
 					/>
 					<Route
 						path="countries"
