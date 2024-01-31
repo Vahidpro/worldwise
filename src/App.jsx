0081e1a0825bc830fd559b6fc3454d68c5147791
Vahidpro <ivahidp@gmail.com>
2024-01-31 15:45:33 +0330

    Implemented login

--- src/App.jsx
+++ src/App.jsx
@@ -10,65 +10,68 @@ import City from "./components/City";
 import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
 import CountryList from "./components/CountryList";
+import { AuthProvider } from "./contexts/FakeAuthContext";
 
 function App() {
 	return (
-		<CitiesProvider>
-			<BrowserRouter>
-				<Routes>
-					<Route
-						index
-						element={<Homepage />}
-					/>
-					<Route
-						path="pricing"
-						element={<Pricing />}
-					/>
-					<Route
-						path="product"
-						element={<Product />}
-					/>
-					<Route
-						path="login"
-						element={<Login />}
-					/>
-					<Route
-						path="app"
-						element={<AppLayout />}
-					>
+		<AuthProvider>
+			<CitiesProvider>
+				<BrowserRouter>
+					<Routes>
 						<Route
 							index
-							element={
-								<Navigate
-									replace
-									to="cities"
-								/>
-							}
+							element={<Homepage />}
 						/>
 						<Route
-							path="cities"
-							element={<CityList />}
+							path="pricing"
+							element={<Pricing />}
 						/>
 						<Route
-							path="cities/:id"
-							element={<City />}
+							path="product"
+							element={<Product />}
 						/>
 						<Route
-							path="countries"
-							element={<CountryList />}
+							path="login"
+							element={<Login />}
 						/>
 						<Route
-							path="form"
-							element={<Form />}
-						/>
-					</Route>
-					<Route
-						path="*"
-						element={<PageNotFound />}
-					></Route>
-				</Routes>
-			</BrowserRouter>
-		</CitiesProvider>
+							path="app"
+							element={<AppLayout />}
+						>
+							<Route
+								index
+								element={
+									<Navigate
+										replace
+										to="cities"
+									/>
+								}
+							/>
+							<Route
+								path="cities"
+								element={<CityList />}
+							/>
+							<Route
+								path="cities/:id"
+								element={<City />}
+							/>
+							<Route
+								path="countries"
+								element={<CountryList />}
+							/>
+							<Route
+								path="form"
+								element={<Form />}
+							/>
+						</Route>
+						<Route
+							path="*"
+							element={<PageNotFound />}
+						></Route>
+					</Routes>
+				</BrowserRouter>
+			</CitiesProvider>
+		</AuthProvider>
 	);
 }
 
--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -14,7 +14,7 @@ function reducer(state, action) {
 		case "logout":
 			return { ...state, user: null, isAuthenticated: false };
 		default:
-			throw new Error("Unknown action type");
+			throw new Error("Unknown action");
 	}
 }
 
@@ -32,13 +32,14 @@ function AuthProvider({ children }) {
 	);
 
 	function login(email, password) {
-		if (email === FAKE_USER.email && password === FAKE_USER.password) {
+		if (email === FAKE_USER.email && password === FAKE_USER.password)
 			dispatch({ type: "login", payload: FAKE_USER });
-		}
 	}
+
 	function logout() {
 		dispatch({ type: "logout" });
 	}
+
 	return (
 		<AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
 			{children}
@@ -48,10 +49,8 @@ function AuthProvider({ children }) {
 
 function useAuth() {
 	const context = useContext(AuthContext);
-
 	if (context === undefined)
-		throw new Error("AuthContext was used outside the AuthProvider");
-
+		throw new Error("AuthContext was used outside AuthProvider");
 	return context;
 }
 
